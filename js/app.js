ced6d08fd812600b63f61e7e110e9661e6fa42a5
wperez185 <wperez185@yahoo.com>
2017-07-09 14:11:27 -0400

    Changed breakpoints for media query use

--- js/app.js
+++ js/app.js
@@ -21,6 +21,7 @@ $(document).ready(function () {
         $(".latest").append(story3Img);
 
         $(".latest").append(story4Img);
+        console.log("hello");
         var breakingNewsLink = "";
         data.articles.forEach(function (ele) {
             // Top section of page for breaking news
